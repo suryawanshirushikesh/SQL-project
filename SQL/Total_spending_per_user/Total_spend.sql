96c0e01a3bb1960b7dc586b00145a1d13f6fa38e
Rushikesh Suryawanshi <suryawanshirushikesh1812@gmail.com>
2025-09-10 20:55:48 +0530

    Update and rename gitkeep to Total_spend.sql

---
+++ SQL/Total_spending_per_user/Total_spend.sql
@@ -0,0 +1,8 @@
+SELECT u.user_id, u.name, SUM(t.amount) AS total_spent
+FROM fintech_db.users u
+JOIN fintech_db.accounts a ON u.user_id = a.user_id
+JOIN fintech_db.transactions t ON a.account_id = t.account_id
+WHERE t.transaction_type = 'debit'
+GROUP BY u.user_id, u.name
+ORDER BY total_spent DESC
+LIMIT 10
